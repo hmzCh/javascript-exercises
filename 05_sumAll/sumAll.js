92f91f854a7f8f19f636b9ba8183922d62a280e1
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-10-30 13:31:19 +0000

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(no1, no2) {
+
+    let output = 0
+
+    if (typeof no1 != "number" || typeof no2 != "number") {
+        return "ERROR"
+    }
+
+    if (no1 < 0 || no2 < 0) {
+        return "ERROR"
+    }
+
+    if (no1<no2) {
+        for (i=no1; i<=no2; i++) {
+            output = output + i
+        }
+    }
+    else {
+        for (i=no2; i<=no1; i++) {
+            output = output + i
+        }
+    }
+    
+    return output
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let unrouded = (input-32) * (5/9)
+  return Math.round(unrouded * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let unrouded = input * (9/5) + 32
+  return Math.round(unrouded * 10) / 10
 };
 
 // Do not edit below this line

912d691d0e96b7ff1c1b9a40ef50ad8d1237182a
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-10-30 13:32:09 +0000

    Error where shouldAdd does not change to false

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsToRemove) {
+
+   const outputArray = []
+   //const unwantedItems = [...arguments]
+   const unwantedItems = arguments[1]
+   let shouldAdd = true
+
+    inputArray.forEach((item) => {
+        shouldAdd = true
+        for(i=0;i<unwantedItems.length;i++) {
+            if (item === unwantedItems[i]) {shouldAdd = false}
+        }
+        if (shouldAdd = true) {outputArray.push(item)}
+    });
+
+    return outputArray
 
 };
 

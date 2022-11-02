a1f40a978ff1d91bbb1970e4aa0b941be168e101
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-11-02 15:57:18 +0000

    Complete exercise
    *the terminal freezes when I test is so I don't know if it works

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase()
+    for (i=0; i<input.length; i--) {
+        if (input.charAt(i) !== input.charAt(input.length-1-i)) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
